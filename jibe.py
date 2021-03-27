c125fe2bd8b009085a04db6fa50bddd7995ce797
Austin Kilduff <austin@akilduff.com>
2021-03-27 16:01:52 -0400

    Put filenames into sets that can be subtracted

--- jibe.py
+++ jibe.py
@@ -8,17 +8,32 @@ import sys
 src = sys.argv[1]
 dst = sys.argv[2]
 
+src_root = None
+src_files = []
+for root, directories, files in os.walk(src):
+    if not src_root:
+        src_root = root + '/'
+    for name in files:
+        rel_path = os.path.join(root, name)[len(src_root):]
+        src_files.append(rel_path)
+
+dst_root = None
+dst_files = []
+for root, directories, files in os.walk(dst):
+    if not dst_root:
+        dst_root = root + '/'
+    for name in files:
+        rel_path = os.path.join(root, name)[len(dst_root):]
+        dst_files.append(rel_path)
+
+src_set = set(src_files)
+dst_set = set(dst_files)
+
 # TODO: Files in src but not in dst should be copied from src to dst
 # TODO: Files in dst but not in src should be deleted from dst
 # TODO: Files in both src and dst should be ignored
-for root, directories, files in os.walk(src):
-    for name in files:
-        print(os.path.join(root, name))
-    for name in directories:
-        print(os.path.join(root, name))
+print('To be copied from src', src_root, 'to dst', dst_root)
+print(src_set - dst_set)
 
-for root, directories, files in os.walk(dst):
-    for name in files:
-        print(os.path.join(root, name))
-    for name in directories:
-        print(os.path.join(root, name))
+print('To be deleted from dst', dst_root)
+print(dst_set - src_set)

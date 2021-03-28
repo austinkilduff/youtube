985aed7ec3ad90597e6ef8be27fd690dee80da18
Austin Kilduff <austin@akilduff.com>
2021-03-28 17:23:34 -0400

    Add note to self and remove unneeded rstrip

--- youtube.py
+++ youtube.py
@@ -1,6 +1,7 @@
 # Prompts for a search term then displays the top results from YouTube
 # Work in progress, I'd like to have this be ncurses-like, and tie in with linkhandler:
 # https://github.com/LukeSmithxyz/voidrice/blob/master/.local/bin/linkhandler
+# TODO: https://docs.python.org/3/howto/curses.html
 
 import requests
 import json
@@ -11,7 +12,7 @@ search_term = input()
 r = requests.get('https://www.youtube.com/results?search_query=' + search_term.replace(' ', '+'))
 page_text = r.text
 # Probably too much text for regex to handle efficiently
-results_json_str = page_text.split('var ytInitialData = ')[1].split('</script>')[0].rstrip()
+results_json_str = page_text.split('var ytInitialData = ')[1].split('</script>')[0]
 results_json_dict = json.loads(results_json_str[:-1])
 results_list = results_json_dict['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][1]['shelfRenderer']['content']['verticalListRenderer']['items']
 

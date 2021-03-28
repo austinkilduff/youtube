f8eba7ba8ae0125ddeee55f2186a652e76ea7f44
Austin Kilduff <austin@akilduff.com>
2021-03-28 17:20:20 -0400

    First steps, prompts and displays static results

---
+++ youtube.py
@@ -0,0 +1,27 @@
+# Prompts for a search term then displays the top results from YouTube
+# Work in progress, I'd like to have this be ncurses-like, and tie in with linkhandler:
+# https://github.com/LukeSmithxyz/voidrice/blob/master/.local/bin/linkhandler
+
+import requests
+import json
+
+print("Enter search term: ")
+search_term = input()
+
+r = requests.get('https://www.youtube.com/results?search_query=' + search_term.replace(' ', '+'))
+page_text = r.text
+# Probably too much text for regex to handle efficiently
+results_json_str = page_text.split('var ytInitialData = ')[1].split('</script>')[0].rstrip()
+results_json_dict = json.loads(results_json_str[:-1])
+results_list = results_json_dict['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][1]['shelfRenderer']['content']['verticalListRenderer']['items']
+
+for result in results_list:
+    video = {
+        'video_title': result['videoRenderer']['title']['runs'][0]['text'],
+        'video_url': 'https://www.youtube.com/watch?v=' + result['videoRenderer']['videoId'],
+        'video_author': result['videoRenderer']['ownerText']['runs'][0]['text'],
+        'video_date': result['videoRenderer']['publishedTimeText']['simpleText'],
+        'video_length': result['videoRenderer']['lengthText']['simpleText'],
+        'video_view_count': result['videoRenderer']['viewCountText']['simpleText']
+        }
+    print(video)
